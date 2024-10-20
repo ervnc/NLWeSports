ce92f573a76186359bb4139365fa8a3402960d09
ervnc <risso.evandro@gmail.com>
2024-10-20 17:21:44 -0300

    last commit

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,21 +1,34 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { GAMES } from '../../utils/games';
 
 
 import { styles } from './styles';
 
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.1.107:3333/games')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setGames(data);
+    })
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={ logoImg} style={ styles.logo } />
 
       <Heading title="Encontre seu duo!" subtitle="Selecione o game que desejar jogar..." />
-
-      <FlatList data={ GAMES } keyExtractor={item => item.id} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={ styles.contentList } renderItem={({ item }) => (
+    
+      <FlatList data={ games } keyExtractor={item => item.id} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={ styles.contentList } renderItem={({ item }) => (
         <GameCard data={ item } />
       )} />
 
---
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,7 @@
+export function convertHourStringToMinutes(hourString: String) {
+    const [hours, minutes] = hourString.split(':').map(Number);
+
+    const minutesAmout = (hours * 60) + minutes;
+
+    return minutesAmout;
+}
